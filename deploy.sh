a6fa64521cccc4b73c93cd7c640d735ba6bede7a
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-02-15 23:06:37 +0100

    fix for strange ipfs name key syntax

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ rm -rf public
 hugo
 OUTPUT=$(ipfs add -q -r public | tail -1)
 ipfs pin add $OUTPUT
-ipfs name publish -k=~/keys/proven-website-key-rsa4096.key $OUTPUT
+ipfs name publish -k=provenwebsitekey $OUTPUT
