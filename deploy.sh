d806e4f5a38cbb0c29aaae9abe62dca78486de90
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-02-18 19:21:57 +0100

    set up for Github Pages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-rm -rf public
-hugo
-OUTPUT=$(ipfs add -q -r public | tail -1)
-ipfs pin add $OUTPUT
-ipfs name publish -k=provennewwebsitekey $OUTPUT
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
